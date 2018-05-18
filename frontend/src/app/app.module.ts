9fe6b7105a978b42728f76fc48765e6656fd941b
Name <name@name.com>
2018-05-19 01:45:13 +0500

    angular_initial

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule} from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {AuthService} from "./services/auth.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path : 'login', component: LoginComponent }
+    ])
+  ],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
